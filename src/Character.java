86de8b82cc3250fe98646f67e09a2bcf8d4d4c52
costnerjc <costnerjc@appstate.edu>
2015-10-08 16:15:38 -0400

    Adds private fields and getters/setters

--- src/Character.java
+++ src/Character.java
@@ -1,8 +1,25 @@
-
-public class Character {
-	public static void main(String[] args)
-	{
-		System.out.println("This is the Character class.");
+public class Character
+{
+	private int healthVal;
+	private int attackVal;
+	private int defenseVal;
+	
+	public int getHealthVal() {
+		return healthVal;
 	}
-
+	public void setHealthVal(int healthVal) {
+		this.healthVal = healthVal;
+	}
+	public int getAttackVal() {
+		return attackVal;
+	}
+	public void setAttackVal(int attackVal) {
+		this.attackVal = attackVal;
+	}
+	public int getDefenseVal() {
+		return defenseVal;
+	}
+	public void setDefenseVal(int defenseVal) {
+		this.defenseVal = defenseVal;
+	}
 }
