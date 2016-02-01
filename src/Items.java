0f64f4ecb29a0e6a6edde2ddd2c5ee0bbb867a4d
basilicedj <basilicedj@appstate.edu>
2016-02-01 17:51:26 -0500

    Added enumerated types

--- src/Items.java
+++ src/Items.java
@@ -1,9 +1,20 @@
 
 
 public class Items {
-public static void main(String[] args)
-{
+    public static void main(String[] args)
+        {
 	System.out.println("This is the items class");
+        }
+    public enum medItem {
+        MEDKIT, MARSPIZZA, MARSBAR, MARSTM, MILKYWAY
+    }
+    public enum progItem {
+        OXYGENTANK, SPACEBIKE, GRAVITYSHOES, HELMET
+    }
+    public enum playerItem {
+        CROWBAR, HAMMER, RAYGUN, HULAKNIFE, CLOTHAR, CHAINAR,
+            DIAMONDAR, ROCK, BROCK, BGROCK, METALROD, MOONSHOES
+    }
+    
 }
 
-}
--- src2/Level.java
+++
@@ -1,8 +0,0 @@
-
-public class Level {
-	public static void main(String[] args)
-	{
-		System.out.println("This is the Level class.");
-	}
-
-}
