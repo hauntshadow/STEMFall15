3b11be99172d3065fdd3f30c70393ad97ee0aa0e
costnerjc <costnerjc@appstate.edu>
2016-02-29 18:05:27 -0500

    Added Scene 2

--- src/StoryText.java
+++ src/StoryText.java
@@ -9,7 +9,7 @@ public class StoryText
         dummyvalue += "[T-MONEY]I've missed you so much... its been so long...\n";
         dummyvalue += "[T-MONEY]good luck on your space mission thing...\n";
         dummyvalue += "[T-MONEY]" + Game.name + ", if I don't see you again I...\n";
-        dummyvalue += "[*KerChow bam PACHING BOOOOOOOOOOOOM*]\n";
+        dummyvalue += "*KerChow bam PACHING BOOOOOOOOOOOOM*\n";
         dummyvalue += "You wake up in a crashed spaceship, plz escape.\n";
         typerPrint(dummyvalue);
     }
@@ -20,7 +20,7 @@ public class StoryText
         dummyvalue += "You emerge, scathed and scared...\n";
         dummyvalue += "Looking around, you see no other human life. Desperately adjusting your transmitter,\n";
         dummyvalue += " you are greeted with nothing but static and the sound of well cooked spaghetti.\n";
-        dummyvalue += "Bored, you start to read your database I textbook.\n";
+        dummyvalue += "Bored, you start to read your Database I textbook.\n";
         dummyvalue += "After 255 seconds, your vision goes blurry, and the once barren wasteland turns into a color\n";
         dummyvalue += "that no one has ever seen before, with DR. T in the foreground!\n";
         dummyvalue += "'" + Game.name + "!' says he. 'I know you are far away right now, but NEVER give up hope'\n";
@@ -32,17 +32,12 @@ public class StoryText
     }
     public static String level2()
     {
-        dummyvalue = "";
-        dummyvalue += "";
-        dummyvalue += "";
-        dummyvalue += "";
-        dummyvalue += "";
-        dummyvalue += "";
-        dummyvalue += "";
-        dummyvalue += "";
-        dummyvalue += "";
-        dummyvalue += "";
-        dummyvalue += "";
+        dummyvalue = "It's unbelievably cold out here. So cold, in fact, that one could say it's \n"
+            + "SUPER COLD. Luckily, your suit has internal heating, so you can't actualy feel it, but the \n"
+            + "seemingly infinite plane of ice sort of gives it away. At this point, you realize two important things:"
+            + "1. There's no way you can survive out in this landscape for more than 8-10 hours.\n"
+            + "2. There's a high probability that you left your oven on at home.\n\n"
+            + "The ship wreckage is thrown about the landscape.\n";
         return dummyvalue;
     }
     public static String level3()
@@ -103,8 +98,16 @@ public class StoryText
                 if (s.charAt(i) == '[')
                 {
                     int end = s.indexOf("]", i + 1);
-                    System.out.print(s.substring(i,end + 1));
+                    System.out.print(s.substring(i, end + 1));
                     i = end;
+                }
+                else if (s.charAt(i) == '*')
+                {
+                    TimeUnit.MILLISECONDS.sleep(1000);
+                    int end = s.indexOf("*", i + 1);
+                    System.out.print(s.substring(i, end + 1));
+                    i = end;
+                    TimeUnit.MILLISECONDS.sleep(1000);
                 }
                 else if (s.charAt(i) == '\n')
                 {
@@ -113,7 +116,7 @@ public class StoryText
                 }
                 else
                 {
-                    TimeUnit.MILLISECONDS.sleep(75);
+                    TimeUnit.MILLISECONDS.sleep(60);
                     System.out.print(s.charAt(i));
                 }
             }
