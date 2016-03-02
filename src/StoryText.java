f295da2a2f3fb0369d3898bb485e61447b19c814
lawingtr <lawingtr@appstate.edu>
2016-03-02 15:38:42 -0500

    spellcheck level3

--- src/StoryText.java
+++ src/StoryText.java
@@ -48,15 +48,15 @@ public class StoryText
         dummyvalue = "";
         dummyvalue += "As you emerge from the fires of the volcano, you look over yourself and the burns you've inevitably sustained.\n";
         dummyvalue += "There's a few minor burns on your arms and hands, but other than that and a few singed hairs on your face, you've made it out alright.\n";
-        dummyvalue += "You look around and begin to notice your surroundings. You don't seem to recognize any of the landmarks that you used for direction just a few hours earlier.\n";
-        dummyvalue += "The eruption has drastically changed the landscape around you. \nThe wreckage of your ship is now covered in magma spewing from the volcano.\n";
+        dummyvalue += "You take a look around and begin to notice your surroundings.\nYou don't seem to recognize any of the landmarks that you used for direction just a few hours earlier.\n";
+        dummyvalue += "The eruption has drastically changed the landscape around you.\nThe wreckage of your ship, usually visible from the peak of the volcano, is now covered in magma spewing from every crevice in the ground.\n";
         dummyvalue += "You make your way down the rest of the slope and gradually begin walking back out into the wasteland that you fear may soon become your grave.\n";
-        dummyvalue += "You find a rock structure in the shape of a chair that looks comfortable enough to sit on\nand begin to contemplate your next plan of action.\n";
-        dummyvalue += "As you look up, you see a mirage of a ship breaking orbit and landing far away from you. It must be the heat coming from the lava paired with dehydration playing tricks on you.\n";
-        dummyvalue += "You recline back and take out your canteen, cherishing the last drops of water that emerge from it.\nYou think of your home back on earth, and realize you've forgotten many things you once held dear.\n";
+        dummyvalue += "You find a rock structure in the shape of a chair that looks comfortable enough to sit on and begin to contemplate your next plan of action.\n";
+        dummyvalue += "As you look up, you see what must be a mirage of a ship breaking orbit and landing far away from you.\nIt must be the heat coming from the lava paired with dehydration playing tricks on your mind.\n";
+        dummyvalue += "You recline back and take out your canteen, cherishing the last few drops of water that emerge from it.\nYou think of your home back on earth, and realize you've forgotten many things you once held dear.\n";
         dummyvalue += "It makes you think of the Mount Doom scene in the third Lord of the Rings movie, after the ring is destroyed.\nAlthough you miss your home, survival is still on your mind, so you begin to think of your next action.\n";
-        dummyvalue += "As you gather yourself and think of the past day's events, you see a hazy vision on the horizon.\n";
-        dummyvalue += "'Could it be?' you ask yourself. As he comes closer and closer, you begin to realize, it is him.\n";
+        dummyvalue += "As you gather yourself and think of the past day's events, you see a hazy vision approaching on the horizon.\n";
+        dummyvalue += "'Could it be?' you ask yourself.\nAs he comes closer and closer, you begin to realize, it is him.\n";
         typerPrint(dummyvalue);
     }
     public static void outro()
